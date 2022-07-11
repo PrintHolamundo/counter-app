2f0887d020ea79b4cd98b8d9b7188dd015eba78d
PrintHolamundo <jdohca@gmail.com>
2022-07-11 12:32:29 -0500

    Colocar Estilos CSS globales

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp.jsx';
 import { HelloWorldApp } from './HelloWorldApp.jsx';
+import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
